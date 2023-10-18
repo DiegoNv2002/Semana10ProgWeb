c986f0c554c57352ba368389e7ddfada6e139ae4
DiegoNv2002 <143531846+DiegoNv2002@users.noreply.github.com>
2023-10-18 15:51:50 -0500

    Add files via upload

---
+++ datos.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Nombre: "; echo $_POST['nombre']; echo "<br/>";
+    echo "Apellidos: "; echo $_POST['apellidos']; echo "<br/>";
+    echo "E-mail: "; echo $_POST['correo']; echo "<br/>";
+    echo "Genero: "; echo $_POST['sexo']; echo "<br/>";
+    echo "Estado Civil: "; echo $_POST['estado']; echo "<br/>";
+    echo "Comentario: "; echo $_POST['comentario']; echo "<br/>";
---
+++ suma.php
@@ -0,0 +1,8 @@
+<?php
+        $suma="";
+        if(isset($_POST['submit'])){
+            $n1=$_POST['n1'];
+            $n2=$_POST['n2'];
+            $suma=$n1+$n2;
+        }
+        ?>
